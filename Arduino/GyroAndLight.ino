ee909c26cf275cbe53d970970e5dc2771e602333
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2023-03-19 11:59:18 -0500

    Update GyroAndLight.ino

--- Arduino/GyroAndLight.ino
+++ Arduino/GyroAndLight.ino
@@ -8,57 +8,70 @@
 #include <MPU6050_light.h>
 #include <FastLED.h>
 
-#define LED_PIN     6
-#define NUM_LEDS    144
+#define LED_PIN 6
+#define NUM_LEDS 144
 
 MPU6050 mpu(Wire);
 String serialData;
 
 CRGB leds[NUM_LEDS];
 
-void setLeds(int r, int g, int b) {
-  for (int i = 0; i < NUM_LEDS; i++) {
+void setLeds(int r, int g, int b)
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
     leds[i] = CRGB(r, g, b);
   }
   FastLED.show();
 }
-void setup() {
+void setup()
+{
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   Serial.begin(9600);
   Serial.println("Starting");
   Wire.begin();
-  byte status =  mpu.begin();
-  if (status != 0) {
-    setLeds(255,0,0);
-    while (true) {};
+  byte status = mpu.begin();
+  if (status != 0)
+  {
+    setLeds(255, 0, 0);
+    while (true)
+    {
+    };
   }
-  else {
-    setLeds(0,255,0);
+  else
+  {
+    setLeds(0, 255, 0);
     delay(500);
-    setLeds(0,0,0);
+    setLeds(0, 0, 0);
   }
 }
 
-void serialEvent() {
+void serialEvent()
+{
   serialData = Serial.readString();
-  if (serialData == "CONE\n") {
-    setLeds(255,255,0);
+  if (serialData == "CONE\n")
+  {
+    setLeds(255, 255, 0);
   }
-  else if (serialData == "CUBE\n") {
-    setLeds(148,0,211);
+  else if (serialData == "CUBE\n")
+  {
+    setLeds(148, 0, 211);
   }
-  else if (serialData == "NONE\n") {
-    setLeds(0,0,0);
+  else if (serialData == "NONE\n")
+  {
+    setLeds(0, 0, 0);
   }
-  else if (serialData == "GETPITCH\n") {
+  else if (serialData == "GETPITCH\n")
+  {
     Serial.println(mpu.getAngleY());
   }
-  else {
+  else
+  {
     Serial.println("Unknown : " + serialData);
   }
-
 }
 
-void loop() {
+void loop()
+{
   mpu.update();
 }
