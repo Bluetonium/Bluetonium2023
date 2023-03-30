8292527da596d1036d1fe60ef7cc083119f42dd5
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2023-03-29 21:07:52 -0500

    idk

--- Bluetonium/src/main/java/frc/robot/Constants.java
+++ Bluetonium/src/main/java/frc/robot/Constants.java
@@ -43,8 +43,8 @@ public final class Constants {
     public static final double BALCINGKD = 0.0d;
 
     // Misic
-    public static final Port ARDUINO_PORT = SerialPort.Port.kMXP;
+    public static final Port ARDUINO_PORT = SerialPort.Port.kUSB2;
     public static final int LED_PWM_PORT = 0;
-    public static final int NUMBER_OF_LEDS = 144;// change later bc tbh i dont really know
+    public static final int NUMBER_OF_LEDS = 93;// change later bc tbh i dont really know
 
 }
